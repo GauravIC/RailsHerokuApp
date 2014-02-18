9a7635d944369b1ce0fbac1de557861d6f39e176
Gaurav Hunuswadkar <gaurav.h@ideaclay.com>
2014-02-18 14:58:24 +0530

    Rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsHerokuApp::Application.config.secret_key_base = 'f34e0859a070b76b600a299e3a4f867fa0a8a504e2f6b4cd7e94505134be888c851014af87fc5c32441b63fdb5aecd7239c2dbfecbe79c9a08f364fbafd370bc'
